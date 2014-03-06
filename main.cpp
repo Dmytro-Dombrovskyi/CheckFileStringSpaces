871eb024823e435bc825c69e002eea9da18dda8b
dmytro_dombrovskyi <dmytro_dombrovskyi@yahoo.com>
2014-03-06 13:26:04 +0200

    Get two functions: write in file and check for spaces

--- main.cpp
+++ main.cpp
@@ -1,59 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+void WriteInFile(const char* filename, std::string &str1);
+void CheckFileForSpaces(const char* filename);
 
-int main ()
-{
    using std::cout;
    using std::cin;
    using std::endl;
-   using std::ios_base;
 
-   const char* str1 = "Today I	 will   be	go  home     after work!";
-   const char* str2 = "After I     spend time   whith    my	   freiends!";
-
+int main ()
+{
+   std::string str1 = "Today I	 will   be	go  home     after work! \n"
+                        "I will     spend time   whith    my	   freiends! \n";
    // creating the object stream , file and write data into it:
-	 const char* filename1 = "data1.txt";
-   std::fstream inOutFile;
-
-   inOutFile.open(filename1, ios_base::out | ios_base::trunc/*| ios_base::app*/ );
-   if(inOutFile.is_open())
-   {
-      inOutFile << str1 << endl;
-      inOutFile << str2 << endl;
-   }
-   inOutFile.clear();
-   inOutFile.close();
-   cout << "File created!" << endl;
-
-   // Check file for space
-   char ch;
-	 std::string s1;
-   inOutFile.open(filename1, ios_base::in);
-   if(inOutFile.is_open())
-   {
-      while(inOutFile.get(ch))
-      {
-				if(isspace (ch))
-				{
-					if(isspace (inOutFile.peek ())) continue;
-					else s1 += ch;
-				}
-        else s1 += ch;
-      }
-      if(inOutFile.eof()) inOutFile.clear();
-			inOutFile.close ();
-   }
-    // rewrite current file
-    inOutFile.open (filename1, ios_base::out | ios_base::trunc);
-    if(inOutFile.is_open ())
-    {
-       inOutFile << s1;
-       inOutFile.close ();
-    }
-    // print string
-    cout << s1 << endl;
-   cout << "File checked!" << endl;
+   const char* filename1 = "data1.txt";
+   WriteInFile(filename1, str1);
+   CheckFileForSpaces(filename1);
 	// std::cin.get ();
    return 0;
+}
+
+void WriteInFile(const char* filename, std::string &str1)
+{
+   using std::ios_base;
+
+   std::fstream OutFile;
+   OutFile.open(filename, ios_base::out | ios_base::trunc/*| ios_base::app*/ );
+   if(OutFile.is_open())
+      OutFile << str1;
+   OutFile.clear();
+   OutFile.close();
+   cout << "File created!" << endl; // could be comment
+}
+// Check file for space
+void CheckFileForSpaces(const char* filename)
+{
+   char ch;
+	std::string tempString;
+	std::fstream inFile;
+   inFile.open(filename, std::ios_base::in);
+   if(inFile.is_open())
+   {
+      while(inFile.get(ch))
+      {
+         if(isspace (ch))
+         {
+            if(isspace (inFile.peek ())) continue;
+            else tempString += ch;
+         }
+         else tempString += ch;
+      }
+      if(inFile.eof()) inFile.clear();
+			inFile.close ();
+			cout << "File checked!" << endl; // could be comment
+   }
+   // rewrite current file
+   WriteInFile(filename, tempString);
+   // print string
+   //cout << tempString << endl;
 }
