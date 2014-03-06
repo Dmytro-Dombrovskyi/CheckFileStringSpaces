43f7f1649d7e3d5f52e96d1e9880ac60e55fdba0
dmytro_dombrovskyi <dmytro_dombrovskyi@yahoo.com>
2014-03-06 12:03:00 +0200

    finished the job

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main ()
    using std::endl;
    using std::ios_base;
 
-   const char* str1 = "Today I go home after work!";
-   const char* str2 = "After I spend time    whith my freiends!";
-   const char* filename1 = "data1.txt";
-   //const char* filename2 = "data2.txt";
+   const char* str1 = "Today I	 will   be	go  home     after work!";
+   const char* str2 = "After I     spend time   whith    my	   freiends!";
 
    // creating the object stream , file and write data into it:
+	 const char* filename1 = "data1.txt";
    std::fstream inOutFile;
+
    inOutFile.open(filename1, ios_base::out | ios_base::trunc/*| ios_base::app*/ );
    if(inOutFile.is_open())
    {
@@ -25,79 +25,35 @@ int main ()
    inOutFile.clear();
    inOutFile.close();
    cout << "File created!" << endl;
+
    // Check file for space
    char ch;
-   int counter1 = 0;
-  // int counter2 = 0;
-   inOutFile.open(filename1, ios_base::in | ios_base::out);
+	 std::string s1;
+   inOutFile.open(filename1, ios_base::in);
    if(inOutFile.is_open())
    {
-      while (inOutFile.get(ch)) ++counter1; // read symbols
-      if(inOutFile.eof()) inOutFile.clear(); // reset flag eof
-      inOutFile.seekg(0);
-
-      std::string s1;
-      std::string s2;
-      while(inOutFile)
+      while(inOutFile.get(ch))
       {
-         inOutFile.get(ch);
-         if(!isspace(ch))
-         {
-            s1 += ch;
-         }
-         else
-         {
-            if(isspace(inOutFile.peek()))
-            {
-               s2
-            }
-         }
-         //s2 += s1 + " ";
+				if(isspace (ch))
+				{
+					if(isspace (inOutFile.peek ())) continue;
+					else s1 += ch;
+				}
+        else s1 += ch;
       }
-      inOutFile.close();
       if(inOutFile.eof()) inOutFile.clear();
-      cout << s1 << endl;
+			inOutFile.close ();
    }
-
+    // rewrite current file
+    inOutFile.open (filename1, ios_base::out | ios_base::trunc);
+    if(inOutFile.is_open ())
+    {
+       inOutFile << s1;
+       inOutFile.close ();
+    }
+    // print string
+    cout << s1 << endl;
    cout << "File checked!" << endl;
-   cout << "Symbols: " << counter1 << endl;
-
-
-   #if 0
-   char ch;
-   const char endLine = '\n';
-   while(cin.get(ch) ) // завершение по достижении EOF
-   {
-      if (ch != endLine)
-         cout << ch;
-      else
-      {
-         cin.putback(ch); // повторная вставка символа
-         break;
-      }
-   }
-   if (!cin.eof())
-   {
-      cin.get(ch);
-      cout << endl << ch << " is next input character.\n";
-   }
-   else
-   {
-      cout << "End of file reached. \n"; // достигнут конец файла
-      //std::exit(0);
-   }
-   while(cin.peek() !='#') // "заглядывание" вперед
-   {
-      cin.get (ch);
-      cout << ch;
-   }
-   if (!cin.eof ())
-      {
-         cin.get(ch);
-         cout << endl << ch << " is next input character.\n";
-      }
-   else
-      cout << "End of file reached. \n"; // достигнут конец файла
-   #endif
+	// std::cin.get ();
    return 0;
 }
